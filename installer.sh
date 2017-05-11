19dd1127c288fa3491f3e9024387bb40d54bacf9
Hubert Léo <leohub@live.fr>
2017-05-12 01:56:52 +0200

    [+] Local install

--- installer.sh
+++ installer.sh
@@ -11,17 +11,17 @@ git submodule init
 git submodule update
 
 echo "Starting installation of websocketpp"
-cd ./libs/websocketpp/; git pull origin master; rm -rf build; mkdir build; cd build; cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_PATH ..; make; make install
+cd ./libs/websocketpp/; git pull origin master; rm -rf build; mkdir build; cd build; cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_PATH ..; make -j4; make install
 cat install_manifest.txt >> $START/install_manifest.txt
 cd $START
 
 echo "Starting installation of rapidjson"
-cd ./libs/rapidjson/; rm -rf build; mkdir build; cd build; cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_PATH ..; make; make install
+cd ./libs/rapidjson/; rm -rf build; mkdir build; cd build; cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_PATH ..; make -j4; make install
 cat install_manifest.txt >> $START/install_manifest.txt
 cd $START
 
 echo "Starting installation of socket.io"
-cd ./libs/socket.io-client-cpp/; git pull origin master; rm -rf build; mkdir build; cd build; cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_PATH -DCMAKE_CXX_FLAGS="-I $PROJECT_PATH/include" ..; make; make install
-cp -rf lib/ $PROJECT_PATH; cp -rf include/ $PROJECT_PATH
+cd ./libs/socket.io-client-cpp/; git pull origin master; rm -rf build; mkdir build; cd build; cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_PATH -DCMAKE_CXX_FLAGS="-I $PROJECT_PATH/include -L$PROJECT_PATH/lib" ..; make -j4; make install
+cp -rf *.a $PROJECT_PATH/lib; cp -rf include/ $PROJECT_PATH
 cat install_manifest.txt >> $START/install_manifest.txt
 cd $START
