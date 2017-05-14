6e4e596513c2ab513972636775e48a2f9780cc05
Hubert Léo <leohub@live.fr>
2017-05-14 18:12:13 +0200

    Fix installer-socket.io

--- installer-socket.sh
+++ installer-socket.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 START=$PWD
-PROJECT_PATH=$PWD/Indie-Studio
+TMP_PATH=$START/libs-tmp
+PROJECT_PATH=$START/Indie-Studio
 
 echo "Starting Indie-Studion libs installer..."
 sleep 2
@@ -11,17 +12,21 @@ git submodule init
 git submodule update
 
 echo "Starting installation of websocketpp"
-cd ./libs/websocketpp/; git pull origin master; rm -rf build; mkdir build; cd build; cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_PATH ..; make -j4; make install
+cd ./libs/websocketpp/; git pull origin master; rm -rf build; mkdir build; cd build; cmake -DCMAKE_INSTALL_PREFIX=$TMP_PATH ..; make -j4; make install
 cat install_manifest.txt >> $START/install_manifest.txt
 cd $START
 
 echo "Starting installation of rapidjson"
-cd ./libs/rapidjson/; rm -rf build; mkdir build; cd build; cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_PATH ..; make -j4; make install
+cd ./libs/rapidjson/; rm -rf build; mkdir build; cd build; cmake -DCMAKE_INSTALL_PREFIX=$TMP_PATH ..; make -j4; make install
 cat install_manifest.txt >> $START/install_manifest.txt
 cd $START
 
 echo "Starting installation of socket.io"
-cd ./libs/socket.io-client-cpp/; git pull origin master; rm -rf build; mkdir build; cd build; cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_PATH -DCMAKE_CXX_FLAGS="-I $PROJECT_PATH/include -L$PROJECT_PATH/lib" ..; make -j4; make install
-cp -rf *.a $PROJECT_PATH/lib; cp -rf include/ $PROJECT_PATH
+cd ./libs/socket.io-client-cpp/; git pull origin master; rm -rf build; mkdir build; cd build; cmake -DCMAKE_INSTALL_PREFIX=$TMP_PATH -DCMAKE_CXX_FLAGS="-I $TMP_PATH/include -L$TMP_PATH/lib" ..; make -j4; make install
+cp -rf *.a $TMP_PATH/lib; cp -rf include/ $TMP_PATH
 cat install_manifest.txt >> $START/install_manifest.txt
 cd $START
+
+
+mkdir $PROJECT_PATH/lib; cp $TMP_PATH/lib/*.a $PROJECT_PATH/lib/
+rm -rf $TMP_PATH
