36d72eb3e3012684ca47b1a63813f15f53918e4d
Remi Gastaldi <remi.gastaldi@epitech.eu>
2017-05-27 16:24:51 +0200

    [+] Animation at player creation

--- Indie-Studio/src/Player.cpp
+++ Indie-Studio/src/Player.cpp
@@ -5,7 +5,7 @@
 // Login   <remi.gastaldi@epitech.eu>
 //
 // Started on  Thu May 18 14:53:43 2017 gastal_r
-// Last update Sat May 27 16:13:22 2017 gastal_r
+// Last update Sat May 27 16:24:03 2017 gastal_r
 //
 
 #include        "Player.hpp"
@@ -21,6 +21,22 @@ Player::Player(Ogre::SceneManager &sceneMgr, OgreBulletDynamics::DynamicsWorld &
   setPosition(position);
   if (orientation != Ogre::Quaternion::ZERO)
     setOrientation(orientation);
+  switch (status)
+  {
+  case Entity::Status::IMMOBILE :
+    changeAnimation("Idle");
+    break;
+  case Entity::Status::MOVE :
+    changeAnimation("Walk");
+    break;
+  case Entity::Status::ATTACK :
+    break;
+  case Entity::Status::HITTED :
+    break;
+  case Entity::Status::DEAD :
+    break;
+  }
+
 return;
   Ogre::AxisAlignedBox boundingB = _entity->getBoundingBox();
   Ogre::Vector3  size = boundingB.getSize();
